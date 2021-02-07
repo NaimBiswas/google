18b784e5ab9cb6a70993c6ad9fed91cd5dda1261
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-07 10:32:12 +0600

    add firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+   apiKey: "AIzaSyB6DxeeijjzZPoO7wOKDVHYE0C1fsVlMd0",
+   authDomain: "g00gle-naimbiswas.firebaseapp.com",
+   projectId: "g00gle-naimbiswas",
+   storageBucket: "g00gle-naimbiswas.appspot.com",
+   messagingSenderId: "772188500813",
+   appId: "1:772188500813:web:a4c3d6c33a7f3ee519bd04",
+   measurementId: "G-28G78CFT6E"
+};
