dcfcd3bb5c9b12302cac932afcc7f6577e1edbe6
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-08 14:43:33 +0600

    search filed done and create two button

--- src/components/Seacrch.js
+++ src/components/Seacrch.js
@@ -2,14 +2,21 @@ import React from 'react'
 import './search.css'
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
+import { Button } from 'react-bootstrap';
 
 const Seacrch = () => {
    return (
-      <div>
-         <SearchIcon className='search_inputIcon'></SearchIcon>
-         <input type="text" />
-         <MicIcon></MicIcon>
-      </div>
+      <>
+         <div className="search-input">
+            <SearchIcon className='search_inputIcon'></SearchIcon>
+            <input className='input' type="text" />
+            <MicIcon className='search_micIcon'></MicIcon>
+         </div>
+         <div className="search-button  text-center mt-4">
+            <Button  >Google Search</Button>
+            <Button >I'm Felling Lucky</Button>
+         </div>
+      </>
    )
 }
 
