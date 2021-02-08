6f8425fde6aae98669c913f18af82c14622f9f40
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-08 20:48:56 +0600

    deployed app on firebase

--- src/components/Seacrch.js
+++ src/components/Seacrch.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './search.css'
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from 'react-bootstrap';
 
 const Seacrch = () => {
+   const [Input, setInput] = useState('')
+   const search = e => {
+      e.preventDefault();
+      alert('This is Worked ');
+   }
+   console.log(Input);
    return (
-      <>
+      <form>
          <div className="search-input">
             <SearchIcon className='search_inputIcon'></SearchIcon>
-            <input className='input' type="text" />
+            <input value={Input} onChange={e => setInput(e.target.value)} className='input' type="text" />
             <MicIcon className='search_micIcon'></MicIcon>
          </div>
          <div className="search-button  text-center mt-4">
-            <Button variant='outlined'>Google Search</Button>
+            <Button onClick={search} variant='outlined'>Google Search</Button>
             <Button variant='outlined'>I'm Feeling Lucky</Button>
          </div>
-      </>
+      </form>
    )
 }
 
