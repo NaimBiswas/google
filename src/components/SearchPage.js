846030210af5485ebbe340e86e70e23ffc15e008
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-09 11:02:27 +0600

    successfully search help of api

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,8 +1,11 @@
 import React, { Fragment } from 'react'
 import './SearchPage.css'
 import { useStateValue } from '../StateProvider'
+import useGoogleSearch from '../useGoogleSearch'
 const SearchPage = () => {
    const [{ term }, dispatch] = useStateValue()
+   const { data } = useGoogleSearch(term)
+   console.log(data);
    return (
       // https://www.googleapis.com/customsearch/v1?[parameters]
 
