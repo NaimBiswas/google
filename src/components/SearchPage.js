b5503a97ec4499a78c03e8f0a43eb346a27973b7
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-09 10:23:45 +0600

    showing search key

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,10 +1,17 @@
 import React, { Fragment } from 'react'
 import './SearchPage.css'
+import { useStateValue } from '../StateProvider'
 const SearchPage = () => {
+   const [{ term }, dispatch] = useStateValue()
    return (
 
       <div className="SearchPage">
+         <div className="searchPage-header">
+            <h3>{term}</h3>
+         </div>
+         <div className="searchPage-result">
 
+         </div>
       </div>
 
    )
