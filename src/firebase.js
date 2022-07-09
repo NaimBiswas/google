dae099c542044262f15e6241752f7ffdbee0b64a
NaimBiswas <nayeembiswas2@gmail.com>
2022-07-09 12:37:57 +0600

    Commit for push on heroku

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,9 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-   apiKey: "AIzaSyB6DxeeijjzZPoO7wOKDVHYE0C1fsVlMd0",
-   authDomain: "g00gle-naimbiswas.firebaseapp.com",
-   projectId: "g00gle-naimbiswas",
-   storageBucket: "g00gle-naimbiswas.appspot.com",
-   messagingSenderId: "772188500813",
-   appId: "1:772188500813:web:a4c3d6c33a7f3ee519bd04",
-   measurementId: "G-28G78CFT6E"
+  apiKey: "AIzaSyATG2zMvncOCZDbe76B309eRXaFDroyhcM",
+  authDomain: "evocative-tube-316905.firebaseapp.com",
+  projectId: "evocative-tube-316905",
+  storageBucket: "evocative-tube-316905.appspot.com",
+  messagingSenderId: "602540383723",
+  appId: "1:602540383723:web:72c52bcaf896145da84dae",
+  measurementId: "G-D38RD4Z81K",
 };
