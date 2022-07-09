745eefdebf8a39812521379bdea7f3071329620e
NaimBiswas <nayeembiswas2@gmail.com>
2022-07-09 13:02:16 +0600

    Fix google problem for firebase

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyATG2zMvncOCZDbe76B309eRXaFDroyhcM",
-  authDomain: "evocative-tube-316905.firebaseapp.com",
-  projectId: "evocative-tube-316905",
-  storageBucket: "evocative-tube-316905.appspot.com",
-  messagingSenderId: "602540383723",
-  appId: "1:602540383723:web:72c52bcaf896145da84dae",
-  measurementId: "G-D38RD4Z81K",
-};
